64f9f2fd081ed29d9cee3a5fc2e8b78a824a248a
virendra-rajpurohit <virendra_rajpurohit@dell.com>
2021-10-28 13:43:36 -0400

    add out cluster dag

---
+++ dags/example_out_cluster.py
@@ -0,0 +1,57 @@
+"""
+Code that goes along with the Airflow tutorial located at:
+https://github.com/apache/airflow/blob/master/airflow/example_dags/tutorial.py
+"""
+
+from datetime import datetime, timedelta
+
+from airflow import configuration
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.models import DAG
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 1, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(days=30),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('Hello_Kubernetes_Operator_DAG', default_args=default_args, schedule_interval=None)
+
+# namespace = configuration.conf.get("kubernetes","namespace")
+namespace = "nautilus-airflow"
+
+t1 = KubernetesPodOperator(
+    namespace=namespace,
+    image="alpine/k8s:1.20.7",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "$(helm --version)"],
+    name="echo",
+    in_cluster=True,
+    task_id="echo",
+    is_delete_operator_pod=False,
+    dag=dag
+)
+
+t2 = KubernetesPodOperator(
+    namespace=namespace,
+    image="alpine/k8s:1.20.7",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "$(helm --version)"],
+    name="echo",
+    in_cluster=False,
+    cluster_context="james-howlett",
+    config_file="/opt/airflow/.kube/config",
+    task_id="echo",
+    is_delete_operator_pod=False,
+    dag=dag
+)
+t1.set_downstream(t2)
